da58cb3f3de505f6e3ff74be3fd6258d8fe9200b
AbdalluhRajab <abdalluh.mahfos2@gmail.com>
2026-05-18 16:03:05 +0300

    test

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if [ -z "$1" ]; then
     echo "Usage: bash scripts/push.sh \"commit message\""
     exit 1
@@ -7,20 +6,20 @@ fi
 
 MESSAGE="$1"
 
-echo "Current Git user"
+echo "Current Git user:"
 git config user.name
 git config user.email
 
-echo "Pulling latest changes from main"
+echo "Pulling latest changes from main..."
 git pull origin main
 
-echo "Adding changed files"
+echo "Adding changed files..."
 git add -A
 
-echo "Creating commit"
+echo "Creating commit..."
 git commit -m "$MESSAGE"
 
-echo "Pushing to GitHub"
+echo "Pushing to GitHub..."
 git push origin main
 
 echo "Done"
